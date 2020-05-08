6dc26d8402eabfd8677541f04f80616c3bfdda8d
Neeraj Chouhan <>
2020-05-08 20:39:12 +0530

    Updates

---
+++ precache-manifest.a4620b4029218897fb66bf23881fcd07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2451540a4cbb93856dd095e779f59f6",
+    "url": "/simple-react-loader/index.html"
+  },
+  {
+    "revision": "71b96fd7f8413aedbaf9",
+    "url": "/simple-react-loader/static/css/main.2af2edaf.chunk.css"
+  },
+  {
+    "revision": "12341be6ddda90cc1fae",
+    "url": "/simple-react-loader/static/js/2.ec8a4559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-loader/static/js/2.ec8a4559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b96fd7f8413aedbaf9",
+    "url": "/simple-react-loader/static/js/main.ec9cf038.chunk.js"
+  },
+  {
+    "revision": "5db5211524c7ddcb9601",
+    "url": "/simple-react-loader/static/js/runtime-main.dd221948.js"
+  }
+]);
